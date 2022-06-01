a8c5ffd96b8f3211fdddb51604887bbc697f8c57
Petter Hareim <petter.hareim@hkraft.no>
2022-06-01 22:55:09 +0200

    animte bad art (!)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./**/*.html"],
-  theme: {
-    extend: {},
+    theme: {
+    extend: {
+      keyframes: {
+        'move-bg': {
+          to: {
+            backgroundPosition: '400% 0',
+          },
+        },
+      },
+      animation: {
+        'move-bg': 'move-bg 16s infinite linear',
+      },
+    },
   },
   plugins: [],
 }
