bc1f5fe4018993f483c9ec0652176f7c1df81abf
Petter Hareim <petter.hareim@hkraft.no>
2023-02-24 21:21:22 +0100

    these are the facts 🤔

---
+++ api/facts.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch');
+
+module.exports = async (req, res) => {
+	try {
+		const response = await fetch('https://uselessfacts.jsph.pl/api/v2/facts/random?language=en');
+		const data = await response.json();
+		res.status(200).send(data.text);
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('Error fetching useless fact.');
+	}
+}
+
+/**
+ * {
+id: "3281767f77eec4f7babc92abbd2464b4",
+text: "In every episode of Seinfeld there is a Superman somewhere.",
+source: "djtech.net",
+source_url: "http://www.djtech.net/humor/useless_facts.htm",
+language: "en",
+permalink: "https://uselessfacts.jsph.pl/api/v2/facts/3281767f77eec4f7babc92abbd2464b4"
+}
+ */
